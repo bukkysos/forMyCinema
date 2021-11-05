7e629e88cf3ebe0d0e6a6c723959c6ab10af6e2e
bukkysos <chukwuebukaanyanwusos@gmail.com>
2021-11-06 00:52:12 +0100

    Dev environment setup

---
+++ src/components/components.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Button } from './buttons'
+import { Input } from './input'
+
+
+export const Components = () => {
+    return (
+        <div>
+            <h2>Buttons</h2>
+
+            <Button />
+
+            <h2>Inputs</h2>
+
+            <Input />
+
+        </div>
+    )
+}
---
+++ src/pages/home/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Components } from '../../components/components'
+
+export const Home = () => {
+    return (
+        <div>
+            <Components />
+        </div>
+    )
+}
